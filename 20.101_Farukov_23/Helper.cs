1d65e73eb0e1188beb5d626331e81543d736a8cc
Farukov <vladislavfarukov2004@gmail.com>
2023-03-31 15:31:34 +0700

    Добавьте файлы проекта.

---
+++ 20.101_Farukov_23/Helper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20._101_Farukov_23
+{
+    internal class Helper
+    {
+		private static Entity.EntitiesLibrary DBEntities;
+		public static Entity.EntitiesLibrary getContex()
+		{
+			if (DBEntities == null)
+			{
+				DBEntities = new Entity.EntitiesLibrary();
+			}
+			return DBEntities;
+		}
+
+		//Поиск специальности по названию
+		public static int getIdSpeciality(string title)
+		{
+			int id = 0;
+			id = DBEntities.Speciality.Where(spec => spec.Title.ToLower().Contains(title.ToLower())).First().IdSpeciality;
+
+			return id;
+		}
+	}
+}
---
+++ 20.101_Farukov_23/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _20._101_Farukov_23
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private Entity.EntitiesLibrary database = Helper.getContex();
+
+        private void btn_Search_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var listGroups = database.Group.ToList();
+
+                string titleSpec = "15.02.09 Аддитивные технологии";
+                int idSpeciality = Helper.getIdSpeciality(titleSpec); //ID нужной специальности
+
+                listGroups = listGroups.Where(group => group.IdSpeciality.Equals(idSpeciality)).ToList(); //Поиск группы по номеру специальности
+
+                listGroups = listGroups.OrderByDescending(group => group.Title).ToList(); //Сортировка по убыванию
+
+                //Вызов исключения, если нет нужных групп
+                if (listGroups.Count == 0)
+                {
+                    throw new Exception();
+                }
+
+                LoadData.ItemsSource = listGroups; //Вывод результата
+            }
+            catch
+            {
+                //Вывод сообщения
+                MessageBox.Show("Нет нужных групп!", "Информация", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+    }
+}
